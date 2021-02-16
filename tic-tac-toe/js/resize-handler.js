9534db4a45529a807dd7ce2824bd144b2f985787
Mohammed-Aman-Khan <45879026+Mohammed-Aman-Khan@users.noreply.github.com>
2021-02-16 14:52:56 +0530

    Update

--- tic-tac-toe/js/resize-handler.js
+++
@@ -1,9 +0,0 @@
-function setTileHeight() {
-    var tiles = $(".board-tile");
-    tiles.css("height", tiles.children().eq(0).width());
-}
-
-$(document).ready(function() {
-    $(window).on("resize", setTileHeight);
-    setTileHeight();
-});
